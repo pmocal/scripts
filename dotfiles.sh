46e2476610a658a6ffcd7dd43edee59477b1d032
Parthiv Mohan <parthivm@berkeley.edu>
2014-06-07 13:26:59 -0700

    port dotfiles

---
+++ dotfiles.sh
@@ -0,0 +1,28 @@
+#this script allows me to easily install my dotfiles on any machine i use, symlinking them into root and storing away existing dotfiles
+
+echo "Have you already cloned the desired dotfiles into the directory ~/dotfiles on this machine?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+			files="bashrc bash_profile vimrc" #desired dotfiles
+          
+			echo "Moving old dotfiles to directory ~/olddotfiles and renaming them without the leading dot"
+			cd ~
+			mkdir ~/olddotfiles
+			for file in $files; do
+				mv ~/.$file ~/olddotfiles/$file
+			done
+
+			echo "Symlinking new dotfiles, located in ~/dotfiles, to files with leading dot in root"
+			cd ~/dotfiles
+			for file in $files; do
+				ln -s ~/dotfiles/$file ~/.$file
+			done
+            ;;
+         No )
+			echo "Please do that first and then run this script."
+			exit
+			;;
+    esac
+done
+
