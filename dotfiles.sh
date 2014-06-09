843d7f4d6491dfbb4820195706b190953fac1933
Parthiv Mohan <parthivm@berkeley.edu>
2014-06-09 11:00:50 -0700

    fix

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,12 +5,13 @@ select yn in "Yes" "No"; do
     case $yn in
         Yes )
 			files="bashrc bash_profile vimrc" #desired dotfiles
-          
+
 			echo "Moving old dotfiles to directory ~/olddotfiles and renaming them without the leading dot"
 			cd ~
 			mkdir ~/olddotfiles
 			for file in $files; do
-				mv ~/.$file ~/olddotfiles/$file
+				mv ~/.$file ~/olddotfiles/
+                mv ~/olddotfiles/.$file ~/olddotfiles/$file
 			done
 
 			echo "Symlinking new dotfiles, located in ~/dotfiles, to files with leading dot in root"
@@ -18,8 +19,12 @@ select yn in "Yes" "No"; do
 			for file in $files; do
 				ln -s ~/dotfiles/$file ~/.$file
 			done
+
+            source ~/.bashrc
+            source ~/.bash_profile
+
             ;;
-         No )
+        No )
 			echo "Please do that first and then run this script."
 			exit
 			;;
